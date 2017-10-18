f61ac1d17db74e11012d456e67197854deac5426
Tim Kamanin <tim@timonweb.com>
2017-10-18 17:48:39 +0200

    Released 0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='django-bulma',
-    version='0.2.0',
+    version='0.3.0',
     description="""Bulma CSS Framework for Django projects""",
     long_description=readme,
     author='Tim Kamanin',
