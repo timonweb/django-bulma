d0f55b456b37b2a0adce80c2cc3a36ed90be90c7
Tim Kamanin <tim@timonweb.com>
2018-06-10 14:29:34 +0200

    Marks 0.5.1 release

--- setup.py
+++ setup.py
@@ -3,35 +3,32 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='django-bulma',
-    version='0.5.0',
+    name="django-bulma",
+    version="0.5.1",
     description="""Bulma CSS Framework for Django projects""",
     long_description=readme,
-    author='Tim Kamanin',
-    author_email='tim@timonweb.com',
-    url='https://timonweb.com/oss/django-bulma/',
-    packages=[
-        'bulma',
-    ],
+    author="Tim Kamanin",
+    author_email="tim@timonweb.com",
+    url="https://timonweb.com/oss/django-bulma/",
+    packages=["bulma"],
     include_package_data=True,
-    install_requires=[
-    ],
+    install_requires=[],
     license="The MIT License (MIT)",
     zip_safe=False,
-    keywords='django-bulma',
+    keywords="django-bulma",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Utilities',
-        'Environment :: Web Environment',
-        'Framework :: Django',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+        "Environment :: Web Environment",
+        "Framework :: Django",
     ],
 )
