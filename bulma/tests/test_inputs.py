8dcb654ef24b32f0f44a8df8c4772bf4831bb6be
Tim Kamanin <tim@timonweb.com>
2020-01-23 13:25:57 +0100

    Adds input names to tests

--- bulma/tests/test_inputs.py
+++ bulma/tests/test_inputs.py
@@ -12,50 +12,50 @@ COLOR_CHOICES = (
 
 @pytest.mark.parametrize("field,label,tag,attributes", [
     (forms.CharField(), "Name", 'input', {
-        'name': 'input',
+        'name': 'name',
         'class': ['input'],
         'type': 'text'
     }),
     (forms.EmailField(), "Email", 'input', {
-        'name': 'input',
+        'name': 'email',
         'class': ['input'],
         'type': 'email'
     }),
     (forms.CharField(widget=forms.NumberInput()), "Age", 'input', {
-        'name': 'input',
+        'name': 'age',
         'class': ['input'],
         'type': 'number'
     }),
     (forms.CharField(widget=forms.URLInput()), "Url", 'input', {
-        'name': 'input',
+        'name': 'url',
         'class': ['input'],
         'type': 'url'
     }),
     (forms.CharField(widget=forms.PasswordInput()), "Password", 'input', {
-        'name': 'input',
+        'name': 'password',
         'class': ['input'],
         'type': 'password'
     }),
     (forms.ChoiceField(choices=[]), "Select", 'select', {
-        'name': 'input'
+        'name': 'choice'
     }),
     (forms.MultipleChoiceField(choices=[]), "Multi select", 'select', {
-        'name': 'input',
+        'name': 'multichoice',
         'multiple': ""
     }),
     (forms.CharField(widget=forms.Textarea()), "Textarea", 'textarea', {
-        'name': 'input',
+        'name': 'text',
         'class': ['textarea']
     }),
     (forms.BooleanField(), "Checkbox", 'input', {
-        'name': 'input',
+        'name': 'agree',
         'type': 'checkbox'
     }),
     (forms.MultipleChoiceField(
         choices=COLOR_CHOICES,
         widget=forms.CheckboxSelectMultiple()
     ), "Checkboxes", 'input', {
-         'name': 'input',
+         'name': 'items',
          'type': 'checkbox',
          'value': 'red'
      }),
@@ -63,23 +63,23 @@ COLOR_CHOICES = (
         choices=COLOR_CHOICES,
         widget=forms.RadioSelect()
     ), "Radios", 'input', {
-         'name': 'input',
+         'name': 'choice',
          'type': 'radio',
          'value': 'red'
      }),
     (forms.FileField(), "File input", 'input', {
-        'name': 'input',
+        'name': 'upload',
         'type': 'file',
         'class': ['file-input']
     })
 ])
 def test_input_rendering(field, tag, label, attributes):
-    class TestForm(forms.Form):
-        input = field
 
+    class TestForm(forms.Form):
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
-            self.fields['input'].label = label
+            self.fields[attributes['name']] = field
+            self.fields[attributes['name']].label = label
 
     output = render_form(TestForm())
     dom = get_dom(output)
